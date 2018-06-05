a000ac7b5d39542a41181a51f79f66123056acdc
mmmoonie <wangchao_moonie@163.com>
2018-06-06 07:39:47 +0800

    contour and hist

--- pillow.py
+++ pillow.py
@@ -1,12 +1,24 @@
 from PIL import Image
 from pylab import *
 
-im = Image.open('lenna.png')
-
+# im = Image.open('lenna.png')
 # 转灰度图
-imshow(im.convert('L'))
+# imshow(im.convert('L'))
 # 缩略图
 # im.thumbnail((128, 128))
 # imshow(im)
+
+im = array(Image.open('lenna.png').convert('L'))
+
+figure()
+
+gray()
+
+contour(im, origin='image')
+axis('equal')
+axis('off')
+
+figure()
+hist(im.flatten(), 128)
 
 show()
